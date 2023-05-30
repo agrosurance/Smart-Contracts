e2f16ac3cef74d28abd32940bede4cf3810b85fb
Yash Goyal <yashgo0018@gmail.com>
2023-05-30 18:16:14 +0530

    added reward token contract

---
+++ contracts/AgroCoin.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact contact@yashgoyal.dev
+contract AgroCoin is ERC20, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("AgroCoin", "ASC") ERC20Permit("AgroCoin") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Snapshot) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(
+        address account,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+}

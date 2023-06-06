e531fd9874d4f566147d3396ac8b209c110bcee9
Yash Goyal <yashgo0018@gmail.com>
2023-06-07 00:40:33 +0530

    added contract verification code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,40 +2,69 @@ const { networks } = require("../networks")
 const hre = require("hardhat")
 
 const landContractDescription = "Land Contract Description"
-const defaultCropImage = "http://"
+const defaultCropImage = "https://nftstorage.link/ipfs/bafkreid6tmxbk4ufk5qng2jw4pa36c4gv7baxnbtr4cjhsxrfk4mvfxzgu"
+
+const crops = [
+  {
+    name: "Maize",
+    image: "https://nftstorage.link/ipfs/bafkreic3tgssdkvg56dmdj336vr7ayfhxalrctklqb5jijxyuvqfcde454",
+  },
+  {
+    name: "Corn",
+    image: "https://nftstorage.link/ipfs/bafkreihblxxple4za3ab5jil4tly4jbq2rcdgbfa6ijdu6tng4uv53e2l4",
+  },
+  {
+    name: "Carrot",
+    image: "https://nftstorage.link/ipfs/bafkreigwqpdgbdu7q4e6s6vqhxbk6nncbyt3xay4mmei26egymz7fzkdym",
+  },
+]
+
+async function addCrops(landContract) {
+  for (const crop of crops) {
+    await landContract.addCrop(crop.name, crop.image)
+  }
+}
+
+async function deployAndVerify(contractName, args) {
+  const factory = await ethers.getContractFactory(contractName)
+  const contract = await factory.deploy(...args)
+  await contract.deployTransaction.wait(2)
+
+  console.log(`${contractName} is deployed to ${contract.address}`)
+
+  try {
+    await hre.run("verify:verify", {
+      address: contract.address,
+      constructorArguments: args,
+    })
+  } catch (err) {
+    console.log(err)
+  }
+
+  return contract
+}
 
 async function main() {
   const insurancePremiumCalculatorCode = fs.readFileSync("./calculate-premium.js").toString()
   const oracleAddress = networks[hre.network.name]["functionsOracleProxy"]
 
-  const AgroCoin = await ethers.getContractFactory("AgroCoin")
-  const agroCoin = await AgroCoin.deploy()
-  await agroCoin.deployed()
-  console.log(`AgroCoin Contract is deployed to ${agroCoin.address}`)
+  const agroCoin = await deployAndVerify("AgroCoin", [])
 
-  const AgroSuranceLand = await ethers.getContractFactory("AgroSuranceLand")
-  const landContract = await AgroSuranceLand.deploy(landContractDescription, defaultCropImage)
-  await landContract.deployed()
-  console.log(`AgroSuranceLand Contract is deployed to ${landContract.address}`)
+  const landContract = await deployAndVerify("AgroSuranceLand", [landContractDescription, defaultCropImage])
 
-  const FundManager = await ethers.getContractFactory("FundManager")
-  const fundManager = await FundManager.deploy()
-  await fundManager.deployed()
-  console.log(`FundManager contract is deployed to ${fundManager}`)
+  const fundManager = await deployAndVerify("FundManager", [])
 
-  const StakingManager = await ethers.getContractFactory("StakingManager")
-  const stakingManager = await StakingManager.deploy(agroCoin.address, fundManager.address)
-  await stakingManager.deployed()
-  console.log(`StakingManager contract is deployed to ${stakingManager.address}`)
+  const stakingManager = await deployAndVerify("StakingManager", [agroCoin.address, fundManager.address])
 
-  const InsuranceManager = await ethers.getContractFactory("InsuranceManager")
-  const insuranceManager = await InsuranceManager.deploy(
+  const insuranceManager = await deployAndVerify("InsuranceManager", [
     insurancePremiumCalculatorCode,
     landContract.address,
-    oracleAddress
-  )
-  await insuranceManager.deployed()
-  console.log(`InsuranceManager contract is deployed to ${insuranceManager.address}`)
+    fundManager.address,
+    oracleAddress,
+  ])
+
+  // add some crops to the land contract
+  await addCrops(landContract)
 }
 
 main().catch((err) => {
